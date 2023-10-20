ed1a31822afa5ad2d49749b122d12ac2a7dd3b14
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-20 12:27:27 +0530

    Added : Random Coin Flip

--- Basic Java Program/src/com/bridgelabz/BasicJavaProgram.java
+++ Basic Java Program/src/com/bridgelabz/BasicJavaProgram.java
@@ -4,5 +4,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BasicJavaProgram {
+    Scanner sc=new Scanner(System.in);
+    public void flipCoin()
+    {
+        int countOfHeads=0;
+        int countOfTails=0;
+        System.out.println("Enter the number to flip the coin");
+        int numOfFlip=sc.nextInt();
+
+        Random random=new Random();
+
+        for(int i=0;i<numOfFlip;i++) {
+            double Coin = random.nextDouble();
+
+            if (Coin < 0.5) {
+                System.out.println("Tails");
+                countOfTails++;
+            } else {
+                System.out.println("Heads");
+                countOfHeads++;
+            }
+        }
+
+        double h=(double)countOfHeads / numOfFlip*100;
+        double t=(double)countOfTails / numOfFlip*100;
+
+        System.out.println("Number of Heads : " + countOfHeads);
+        System.out.println("Number of Tails : " + countOfTails);
+        System.out.println("Percentage of Head : " + h + " %");
+        System.out.println("Percentage of Tails : " + t + " %");
+    }
 
 }
--- Basic Java Program/src/com/bridgelabz/MainClass.java
+++ Basic Java Program/src/com/bridgelabz/MainClass.java
@@ -7,7 +7,21 @@ public class MainClass {
 
     public static void main(String[] args) {
         BasicJavaProgram program=new BasicJavaProgram();
-        System.out.println("Welcome to basic java program!");
+        System.out.println("============= Welcome to basic java program! =============");
+
+        System.out.println("Enter the Number to select the option");
+        System.out.println("1) Flip Coin and print percentage of Heads and Tails");
+
+        int option=sc.nextInt();
+        switch (option){
+            case 1:
+                program.flipCoin();
+                break;
+
+            default:
+                System.out.println("You entered wrong option");
+                break;
+        }
 
     }
 }
