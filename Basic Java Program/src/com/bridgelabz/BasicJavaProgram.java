d37f901acbbb978e5d525df1554c7e7354cdb3f5
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-20 12:23:03 +0530

    Added Welcome to Basic Java Program

---
+++ Basic Java Program/src/com/bridgelabz/BasicJavaProgram.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BasicJavaProgram {
+
+}
---
+++ Basic Java Program/src/com/bridgelabz/MainClass.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class MainClass {
+   static Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        BasicJavaProgram program=new BasicJavaProgram();
+        System.out.println("Welcome to basic java program!");
+
+    }
+}
